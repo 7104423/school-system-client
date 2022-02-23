2411daf5f977234e0acd088a163e76b8897bd1ca
Jan Janás <Jan.Janas@UnicornUniversity.net>
2022-02-23 15:00:36 +0100

    application initialization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+		</div>
+	);
+}
+
+export default App;
