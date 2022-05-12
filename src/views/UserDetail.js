be7bd55a4bd12332885745228b67c59195a21df1
Olga Dubova <oli.dubova@gmail.com>
2022-05-13 00:11:56 +0200

    grid in userDetaail was added

--- src/views/UserDetail.js
+++ src/views/UserDetail.js
@@ -10,7 +10,20 @@ export const UserDetail = () => {
     <Layout active="users">
       <ControlPanel title={"Nejaky titulek"} id={id} page={"user"} />
       <Grid container>
-        <Grid xs={12}>TODO</Grid>
+        <Grid xs={6}>Email:</Grid>
+        <Grid xs={6}>dan@study.com</Grid>
+      </Grid>
+      <Grid container>
+        <Grid xs={6}>Name:</Grid>
+        <Grid xs={6}>Daniel</Grid>
+      </Grid>
+      <Grid container>
+        <Grid xs={6}>Surname:</Grid>
+        <Grid xs={6}>Black</Grid>
+      </Grid>
+      <Grid container>
+        <Grid xs={6}>Role:</Grid>
+        <Grid xs={6}>Student</Grid>
       </Grid>
     </Layout>
   );
