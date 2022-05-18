a0fe092fd30f4518a57e5cb8728b6855ee7c97ad
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-05-19 00:20:47 +0200

    edit role redirect

--- src/containers/withRole.js
+++ src/containers/withRole.js
@@ -4,10 +4,9 @@ export const withRole = (roles, Component) => (props) => {
   const user = useUser();
   const userRoles = user.getRoles();
   const hasRole = roles.some((role) => userRoles.includes(role));
-  console.log("called");
   if (!hasRole) {
     user.logout();
-    window.location.href = "/app/login";
+    window.location.href = "/app/subjects";
     return null;
   }
   return <Component />;
